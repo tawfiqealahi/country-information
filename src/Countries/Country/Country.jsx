468604aedacf2604da74222b5f06609b1ba8b50f
tawfiq <tawfiqealahi99@gmail.com>
2024-10-14 11:08:30 +0600

    visited place

--- src/Countries/Country/Country.jsx
+++ src/Countries/Country/Country.jsx
@@ -1,13 +1,26 @@
+import { useState } from 'react';
 import './Country.css';
 
 const Country = ({data}) => {
-    const {name,flags,population,cca3} = data;
+    const {name,flags,population,cca3,area} = data;
+
+    const[visited,setVisited]=useState(false);
+    const handleVisited=()=>{
+        setVisited(true);
+    }
+
     return (
         <div className="box">
             <h2>Country Name: {name?.common}</h2>
             <h2>Country Code: {cca3}</h2>
+            <h2>Area: {area}</h2>
             <h2>Population: {population} </h2>
+            <button onClick={handleVisited} >visited</button>
+            {
+                visited ? 'i have aleeady visited' : 'i want to check out this place'
+            } 
             <img src={flags.png} alt="country image"  />
+
 
         </div>
     );
